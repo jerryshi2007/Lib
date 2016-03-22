0359283f8353a950321f005d1239f47f4676ebac
jerryshi2007 <jerryshi2007@outlook.com>
2016-03-22 23:04:45 +0800

    move

---
+++ Lib.Cache/Base/CacheKey.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lib.Cache
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    internal class CacheKey
+    {
+        internal CacheKey(String key)
+        {
+            _key = key;
+            _hash = key.GetHashCode();
+        }         
+
+        private String _key;
+        private int _hash;
+        protected byte _bits;
+
+        internal int Hash
+        {
+            get { return _hash; }
+        }
+        internal String Key
+        {
+            get { return _key; }
+        }
+
+         
+    }
+}
